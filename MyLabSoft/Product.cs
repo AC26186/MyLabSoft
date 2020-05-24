a010b7512d47319adb9a4fe27a38483505e20a81
ac26186 <pushkarsingh.adhikari@centurylink.com>
2020-05-24 19:38:44 +0530

    Test3

--- MyLabSoft/Product.cs
+++ MyLabSoft/Product.cs
@@ -10,11 +10,7 @@ namespace MyLabSoft
         public int ProductID { get; set; }
         public string ProductName { get; set; }
         public string ProductCode { get; set; }
-        public string IsourceCatalogId { get; set; }
-        public string WorkSheetName { get; set; }
         public bool IsActive { get; set; }
-        public short EndPoints { get; set; }
-        public string ApplicableRules { get; set; }
         public string CreateId { get; set; }
         public DateTime CreateDate { get; set; }
         public string UpdateId { get; set; }
